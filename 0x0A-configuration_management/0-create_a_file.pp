404c5648a7c691587e8100e9a095b9be86bf44b0
Njiraini23 <njiraini88@gmail.com>
2023-04-30 11:30:48 +0000

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# A file in /tmp/school using Puppet
+file { 'school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
