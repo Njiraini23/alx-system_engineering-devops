fbc02fe6bef5d6c471ed06cf45a81211668ca2a0
Njiraini23 <njiraini88@gmail.com>
2023-04-30 12:11:25 +0000

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
+#!/usr/bin/pup
 #installing flask from pip3 using Puppet
-
-package { 'puppet-lint':
-ensure   => '2.5.0',
-provider => 'gem',
-source   => 'https://rubygems.org'
+package {'flask':
+  ensure   => '2.5.0',
+  provider => 'pip3'
 }
