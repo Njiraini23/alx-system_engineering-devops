493f5d2c29b10f6cdcf5a9e2e66da935d66f0040
Njiraini23 <njirainii88@gmail.com>
2023-05-09 06:42:07 +0000

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 #make changes to configuration file
-file_line { 'Use private key':
-ensure => 'present',
-path   => 'IdentityFile ~/.ssh/school'
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'Avoid password':
-ensure => 'present',
-path   => '/etc/ssh/ssh_config',
-line   => 'PasswordAuthentification no'
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
