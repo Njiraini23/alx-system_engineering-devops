e939115b52dd666877223006ad25504e5223e179
Njiraini23 <njirainii88@gmail.com>
2023-05-30 16:39:55 +0000

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+""" Calling to an api and etting todos task of an specific user """
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+if __name__ == '__main__':
+    user = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]),
+        verify=False).json()
+    todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+            sys.argv[1]),
+        verify=False).json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    complete = [task.get('title')
+                for task in todos if task.get('completed')]
+    print(
+        'Employee {} is done with tasks({}/{}):'.
+        format(user['name'],
+               complete.__len__(),
+               todos.__len__()))
+    print("\n".join("\t {}".format(task) for task in complete))
