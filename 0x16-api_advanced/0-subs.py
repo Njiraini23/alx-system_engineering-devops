7677f7cc1162f0aa76d2c24864a37ec087d42d6c
Njiraini23 <njiraini88@gmail.com>
2023-06-13 09:47:02 +0000

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,13 +2,20 @@
 """Queries the Reddit API and returns the number of subsrcibers
 for a given subreddit
 """
+
+
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    agent = 'mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0)'
-    agent += ' Gecko/20100101 Firefox/47.0'
-    headers = {"user-Agent": agent}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    return 0 if response.status_code != 200 else response.json()\
-            .get('data')\
-            .get('subscribers')
+    headers = {'User-Agent': 'mozilla/5.0'}
+
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code = 200:
+            data = response.json()
+            subscribers = data.get('data').get('subscribers')
+            return int(subscribers)
+        else:
+            return 0
+    except requests.exceptions.RequestException:
+        return 0
