665582bfbe36e007813b2d4398ce9bbec034a76c
Njiraini23 <njiraini88@gmail.com>
2023-06-13 09:55:41 +0000

    changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,16 +2,17 @@
 """Queries the Reddit API and returns the number of subsrcibers
 for a given subreddit
 """
+import requests
 
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'mozilla/5.0'}
 
     try:
         response = requests.get(url, headers=headers)
-        if response.status_code = 200:
+        if response.status_code == 200:
             data = response.json()
             subscribers = data.get('data').get('subscribers')
             return int(subscribers)
