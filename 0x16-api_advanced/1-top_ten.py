61a0a55a45912905d650e5c75f25dffde9249d4f
Njiraini23 <njiraini88@gmail.com>
2023-06-13 10:38:13 +0000

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" querie sthe Reddit API and prints titles of 
+the first 10 hot posts for a given subreddit """
+
+
+def top_ten(subreddit):
+    """querries the Reddit API and prints the 
+    titles of the first 10 hot posts """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    try:
+        response = request.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            children = data.get('data').get('children')
+            for i in range(10):
+                print(children[i].get('data').get('title'))
+        else:
+            print('None')
+    except request.exceptions.RequestException:
+        print('None')
+
